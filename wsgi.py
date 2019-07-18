e64024882bf2d05d7ef84f99ad20674f33a82870
Dinesh <janepelladinesh97@gmail.com>
2019-07-18 13:47:48 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from kafka import KafkaConsumer
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    consumer = KafkaConsumer(bootstrap_servers='10.128.0.202:9092',
+                                 auto_offset_reset='earliest',
+                                 consumer_timeout_ms=1000)
+    consumer.subscribe(['test'])
+    consumer.close()
     return "Hello World!"
 
 if __name__ == "__main__":
