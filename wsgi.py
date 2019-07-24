0f8e44b8ce02eb625953015bec16f6d2e3100f85
Dinesh <janepelladinesh97@gmail.com>
2019-07-24 14:32:14 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,11 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    consumer = KafkaConsumer(bootstrap_servers='victoria.com:6667',
-                                 auto_offset_reset='earliest',
-                                 consumer_timeout_ms=1000)
-    consumer.subscribe(['my-topic'])
-    consumer.close()
+    
     return "Hello World!"
 
 if __name__ == "__main__":
