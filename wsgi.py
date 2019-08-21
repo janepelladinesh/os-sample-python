4f257f14b12f4722237ffdac01bca2153b441d5a
Dinesh <janepelladinesh97@gmail.com>
2019-08-21 18:28:38 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,13 @@
 from flask import Flask
-
+import os
 
 application = Flask(__name__)
 
+workers = os.environ.get('GUNS', 'default')
 @application.route("/")
 def hello():
     
-    return "Hello World!"
+    return workers
 
 if __name__ == "__main__":
     application.run()
